ed2f85e4fc4c25aa7482d65be939dea381451fb7
Adilson Batista <adbsilvajr@gmail.com>
2019-05-01 16:02:32 -0300

    conectando com servidor smtp da google

--- src/main/java/com/adbsilvajr/cursomc/services/DBService.java
+++ src/main/java/com/adbsilvajr/cursomc/services/DBService.java
@@ -110,7 +110,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "234234", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "adbsilvajr@gmail.com", "234234", TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("123123", "321321"));
 
 		Endereco e1 = new Endereco(null, "Rua Teste", "111", "apt1", "Jardin1", "11111", cli1, c1);
