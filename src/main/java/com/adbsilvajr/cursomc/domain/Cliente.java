6ac54d83576567acc7b8c9590fe496bc068eab0a
Adilson Batista <adbsilvajr@gmail.com>
2019-05-01 19:50:32 -0300

    Criando perfil para os clientes

--- src/main/java/com/adbsilvajr/cursomc/domain/Cliente.java
+++ src/main/java/com/adbsilvajr/cursomc/domain/Cliente.java
@@ -5,17 +5,20 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.adbsilvajr.cursomc.domain.enums.Perfil;
 import com.adbsilvajr.cursomc.domain.enums.TipoCliente;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -24,31 +27,35 @@ public class Cliente implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-
-	@Column(unique = true)
+	
+	@Column(unique=true)
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
-
+	
 	@JsonIgnore
 	private String senha;
-
-	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+	
+	@OneToMany(mappedBy="cliente", cascade=CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
-
+	
 	@ElementCollection
-	@CollectionTable(name = "TELEFONE")
+	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
-
+	
+	@ElementCollection(fetch=FetchType.EAGER)
+	@CollectionTable(name="PERFIS")
+	private Set<Integer> perfis = new HashSet<>();
+	
 	@JsonIgnore
-	@OneToMany(mappedBy = "cliente")
+	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
-
+	
 	public Cliente() {
-
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo, String senha) {
@@ -57,68 +64,77 @@ public class Cliente implements Serializable {
 		this.nome = nome;
 		this.email = email;
 		this.cpfOuCnpj = cpfOuCnpj;
-		this.tipo = (tipo == null) ? null : tipo.getCod();
+		this.tipo = (tipo==null) ? null : tipo.getCod();
 		this.senha = senha;
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Integer getId() {
 		return id;
 	}
 
-	public String getNome() {
-		return nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public String getCpfOuCnpj() {
-		return cpfOuCnpj;
-	}
-
-	public TipoCliente getTipo() {
-		return TipoCliente.toEnum(tipo);
-	}
-
-	public List<Endereco> getEnderecos() {
-		return enderecos;
-	}
-
-	public Set<String> getTelefones() {
-		return telefones;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
 	public void setId(Integer id) {
 		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
 	}
 
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
+	public String getEmail() {
+		return email;
+	}
+
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public String getCpfOuCnpj() {
+		return cpfOuCnpj;
 	}
 
 	public void setCpfOuCnpj(String cpfOuCnpj) {
 		this.cpfOuCnpj = cpfOuCnpj;
 	}
 
+	public TipoCliente getTipo() {
+		return TipoCliente.toEnum(tipo);
+	}
+
 	public void setTipo(TipoCliente tipo) {
 		this.tipo = tipo.getCod();
 	}
 
+	public String getSenha() {
+		return senha;
+	}
+	
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	public Set<Perfil> getPerfis() {
+		return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+	}
+	
+	public void addPerfil(Perfil perfil) {
+		perfis.add(perfil.getCod());
+	}
+	
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+
 	public void setEnderecos(List<Endereco> enderecos) {
 		this.enderecos = enderecos;
+	}
+
+	public Set<String> getTelefones() {
+		return telefones;
 	}
 
 	public void setTelefones(Set<String> telefones) {
@@ -132,7 +148,7 @@ public class Cliente implements Serializable {
 	public void setPedidos(List<Pedido> pedidos) {
 		this.pedidos = pedidos;
 	}
-
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -156,5 +172,5 @@ public class Cliente implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
+	}	
 }
--- src/main/java/com/adbsilvajr/cursomc/services/DBService.java
+++ src/main/java/com/adbsilvajr/cursomc/services/DBService.java
@@ -20,6 +20,7 @@ import com.adbsilvajr.cursomc.domain.PagamentoComCartao;
 import com.adbsilvajr.cursomc.domain.Pedido;
 import com.adbsilvajr.cursomc.domain.Produto;
 import com.adbsilvajr.cursomc.domain.enums.EstadoPagamento;
+import com.adbsilvajr.cursomc.domain.enums.Perfil;
 import com.adbsilvajr.cursomc.domain.enums.TipoCliente;
 import com.adbsilvajr.cursomc.repositories.CategoriaRepository;
 import com.adbsilvajr.cursomc.repositories.CidadeRepository;
@@ -36,7 +37,7 @@ public class DBService {
 
 	@Autowired
 	private BCryptPasswordEncoder pe;
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	@Autowired
@@ -114,16 +115,25 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "adbsilvajr@gmail.com", "234234", TipoCliente.PESSOAFISICA,pe.encode("123"));
+		Cliente cli1 = new Cliente(null, "Maria Silva", "adbsilvajr@gmail.com", "234234", TipoCliente.PESSOAFISICA,
+				pe.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("123123", "321321"));
+
+		Cliente cli2 = new Cliente(null, "ADILSON Silva", "teste@gmail.com", "234234", TipoCliente.PESSOAFISICA,
+				pe.encode("123"));
+		cli2.getTelefones().addAll(Arrays.asList("123123", "321321"));
+		cli2.addPerfil(Perfil.ADMIN);
 
 		Endereco e1 = new Endereco(null, "Rua Teste", "111", "apt1", "Jardin1", "11111", cli1, c1);
 		Endereco e2 = new Endereco(null, "Rua Teste2", "222", "apt2", "Jardin2", "22222", cli1, c2);
+		Endereco e3 = new Endereco(null, "Rua Teste3", "3", "apt3", "Jardi3n3", "33333", cli2, c2);
 
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
 
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
+
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
