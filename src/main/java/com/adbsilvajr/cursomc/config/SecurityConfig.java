a486e026278a01a9d4bc7ffc2b743899833ba98c
Adilson Batista <adbsilvajr@gmail.com>
2019-05-02 21:24:29 -0300

    implementando autorizacao validando o token jwt com controle de expiracao de sessao

--- src/main/java/com/adbsilvajr/cursomc/config/SecurityConfig.java
+++ src/main/java/com/adbsilvajr/cursomc/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.adbsilvajr.cursomc.sercurity.JWTAuthenticationFilter;
+import com.adbsilvajr.cursomc.sercurity.JWTAuthorizationFilter;
 import com.adbsilvajr.cursomc.sercurity.JWTUtil;
 
 @Configuration
@@ -54,8 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().antMatchers(PUBLIC_MATCHERS).permitAll()
 				.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
-		// http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil,
-		// userDetailsService));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
